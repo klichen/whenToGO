43a24121ddd88318442ee15b2236dfc853875d0f
klichen <kevin60342@gmail.com>
2024-09-26 11:56:50 -0400

    setup tailwind eslint and prettier

---
+++ client/src/app/App.tsx
@@ -0,0 +1,15 @@
+// import './App.css';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-3lg font-bold italic text-opacity-50">
+        HELLO WORLD!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
